1517d795dc67ae9d4a53322618cf5c422bb6e5c8
Kyuu <public.kyuuanago@gmail.com>
2023-05-07 23:03:10 +0900

    [update] cors

--- backend-graphql/config/initializers/cors.rb
+++ backend-graphql/config/initializers/cors.rb
@@ -5,12 +5,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "jelly.nattoujam.home:3000","jelly-fish.local:3000"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
