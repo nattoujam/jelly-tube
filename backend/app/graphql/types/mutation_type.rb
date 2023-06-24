b293a5269b1205ff379d1ec6050cc7f0c788ff23
Kyuu <public.kyuuanago@gmail.com>
2023-06-24 14:17:11 +0900

    [add] createTag mutation

--- backend/app/graphql/types/mutation_type.rb
+++ backend/app/graphql/types/mutation_type.rb
@@ -6,5 +6,7 @@ module Types
       description: "指定したVideoを編集"
     field :deleteVideo, mutation: Mutations::DeleteVideo,
       description: "指定したVideoを削除"
+    field :createTag, mutation: Mutations::CreateTag,
+      description: "新しいTagを追加"
   end
 end
