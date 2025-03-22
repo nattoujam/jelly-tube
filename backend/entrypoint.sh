a683301d7c89ac01e8c312ae444a7f336da2fd50
nattoujam <Public.kyuuanago@gmail.com>
2025-03-23 02:01:38 +0900

    patch-chore: backend image用のdockerfileを追加

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
-# File              : entrypoint.sh
-# Author            : nattoujam <public.kyuuanago@gmail.com>
-# Date              : 2023 04/28
-# Last Modified Date: 2023 04/28
-# Last Modified By  : nattoujam <public.kyuuanago@gmail.com>
+#!/bin/bash -e
 
-set -e
-rm -f /app/tmp/pids/server.pid
-exec "$@"
+# Enable jemalloc for reduced memory usage and latency.
+if [ -z "${LD_PRELOAD+x}" ]; then
+    LD_PRELOAD=$(find /usr/lib -name libjemalloc.so.2 -print -quit)
+    export LD_PRELOAD
+fi
+
+# If running the rails server then create or migrate existing database
+if [ "${@: -2:1}" == "./bin/rails" ] && [ "${@: -1:1}" == "server" ]; then
+  ./bin/rails db:prepare
+fi
+
+exec "${@}"
